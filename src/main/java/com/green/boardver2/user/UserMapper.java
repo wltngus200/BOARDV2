5c4f0514dc6a9b90117dfec6b96215bf13a97e7c
Administrator <wltngus200@gmail.com>
2024-04-27 15:18:42 +0900

    2024 04 26

--- src/main/java/com/green/boardver2/user/UserMapper.java
+++ src/main/java/com/green/boardver2/user/UserMapper.java
@@ -9,7 +9,8 @@ import org.apache.ibatis.annotations.Mapper;
 public interface UserMapper {
     int postUser(UserPostReq p);
     //insert 전용의 멤버필드
-    UserEntity getUserById(String uid);
+    UserEntity getUserById(String uid);//Mapper.xml
+    //.xml에 있는 쿼리문에 따라 기능을 수행 uid에 입력된 값을 가진 데이터를 가져와 소장
     int patchPassword(ChangePasswordPatchReq p);
 
 }
